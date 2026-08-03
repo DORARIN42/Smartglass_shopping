c5f10510bb87b72788e4ee9eb2e4df43f4fa70e4
yui <147612146+ChoYui@users.noreply.github.com>
2026-08-03 10:52:31 +0900

    UI 추가

---
+++ app/build/generated/source/buildConfig/release/com/example/raybanvision/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.raybanvision;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.example.raybanvision";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String ANALYSIS_BASE_URL = "http://172.30.1.99:8080/";
+  // Field from default config.
+  public static final Boolean USE_MOCK_GLASSES = false;
+}
